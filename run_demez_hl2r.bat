0f836323160abddf050b35196478b3b7d412f41c
demizexp <demizexp@gmail.com>
2019-02-23 01:37:30 -0500

    updated gameinfo and batch file

--- run_demez_hl2r.bat
+++ run_demez_hl2r.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set cmdLine=-w 1900 -h 1000 -console -dev -novid -sw -game hl2r
+set cmdLine=-w 1905 -h 1000 -console -dev -novid -sw -game hl2r
 
 "../quiver.exe" %cmdLine%
 echo "../quiver.exe" %cmdLine%
